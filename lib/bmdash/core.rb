73f0411108d2303e19f7d5c8204908f5c115a8ab
Paul Rose <mail@paulrose.me>
2015-07-29 15:15:12 +0100

    Added Scheduler and events enpoint

--- lib/bmdash/core.rb
+++ lib/bmdash/core.rb
@@ -1,17 +1,19 @@
 require 'sinatra'
 require 'sprockets'
 require 'sass'
+require 'rufus-scheduler'
+require 'pp'
 
 
 module Bytemark
     class BMDash < Sinatra::Base
-        
 
         configure do 
             # Setup defaults 
             set :root, Dir.pwd
             set :public_folder, File.join(settings.root, 'public')
             set :assets, Sprockets::Environment.new(settings.root)
+            set :scheduler, Rufus::Scheduler.new 
             set :connections, []
             set :server, 'thin'
             # Enable sweet things
@@ -21,6 +23,18 @@ module Bytemark
                settings.assets.append_path("./assets/#{path}")
             end
             manifest = Sprockets::Manifest.new(assets, './public/assets/manifest.json').save
+
+            scheduler.every '1s' do 
+               ping_clients
+            end
+
+        end
+
+
+        def self.ping_clients 
+            self.connections.each do |client|
+                client << "Hello!"
+            end
         end
 
         configure :development do 
@@ -38,9 +52,17 @@ module Bytemark
              asset
         end 
 
+        get '/events', provides: 'text/event-stream' do
+          pp params
+          stream :keep_open do |out|
+            settings.connections << out
+          end
+        end
+
         get '/' do 
             'BMDash! is here'
         end
+
 
     end
 end
