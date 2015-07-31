f03baa399bfe4184d4de7b4ad94df69eabd97c6f
Paul Rose <mail@paulrose.me>
2015-07-31 14:58:15 +0100

    Added file watching and client connection code

--- lib/bmdash/core.rb
+++ lib/bmdash/core.rb
@@ -2,38 +2,95 @@ require 'sinatra'
 require 'sprockets'
 require 'sass'
 require 'rufus-scheduler'
+require 'filewatcher'
+require 'yaml'
 require 'pp'
 
 
 module Bytemark
     class BMDash < Sinatra::Base
 
+        class ClientConnection
+            attr_reader :name, :ip, :connected_at, :type, :stream
+            def initialize info
+                info.each do |key,value|
+                   instance_variable_set "@#{key}", value
+                end
+            end
+        end
+
+        class Widget 
+            attr_reader :name, :author, :email, :about
+            def initialize about
+                about.each do |key,value|
+                   instance_variable_set "@#{key}", value
+                end
+            end
+        end
+
+        def self.load_widgets
+            Dir.foreach('widgets') do |dir|
+                next if dir[0] == '.'
+                widget_info = File.join Dir.pwd, 'widgets',  dir, 'about.yml'
+                if File.exists? widget_info 
+                   info = YAML::load_file widget_info
+                   self.widgets[info['name']] = Widget.new(info)
+                end
+            end
+            puts 'Available widgets:'
+            self.widgets.each do |name, widget|
+                puts "    - #{name}"
+            end
+
+        end
+
+        def self.file_changed filename, event
+
+        end
+
+        def self.ping_clients 
+            puts 'Currently Connected: '
+            self.connections.each do |client|
+                puts '    - ' + client.name
+                client.stream << "ping!"
+            end
+        end
+
         configure do 
+            # Enable sweet things
+            enable :logging
+
             # Setup defaults 
             set :root, Dir.pwd
             set :public_folder, File.join(settings.root, 'public')
             set :assets, Sprockets::Environment.new(settings.root)
             set :scheduler, Rufus::Scheduler.new 
+            set :watcher, FileWatcher.new(['./widgets', './dashboards'])
+            set :watcher_thread, nil
             set :connections, []
+            set :widgets, {}
             set :server, 'thin'
-            # Enable sweet things
-            enable :logging
-            # Setup asset processing
+            set :logger, ENV['rack.logger'] # Setup asset processing
             %w(javascripts stylesheets fonts images sounds).each do |path|
                settings.assets.append_path("./assets/#{path}")
             end
             manifest = Sprockets::Manifest.new(assets, './public/assets/manifest.json').save
 
+            # Load widgets from ./widgets
+            load_widgets
+
+            # Setup default events
             scheduler.every '5s' do 
                ping_clients
             end
-
-        end
-
-        def self.ping_clients 
-            self.connections.each do |client|
-                client.stream << "ping!"
+            
+            # Setup file watching
+            watcher_thread = Thread.new(watcher) do |watcher|
+                watcher.watch do |filename, event|
+                    self.file_changed filename, event
+                end
             end
+
         end
 
         configure :development do 
@@ -62,7 +119,12 @@ module Bytemark
                 :stream => out
             }
             client = ClientConnection.new info
+            client.stream.callback do 
+                settings.connections.delete(client)
+            end
             settings.connections << client
+            
+            out << "Hello!"
           end
         end
 
@@ -71,12 +133,5 @@ module Bytemark
         end
     end
 
-    class ClientConnection
-        attr_reader :name, :ip, :connected_at, :type, :stream
-        def initialize info
-            info.each do |key,value|
-               instance_variable_set "@#{key}", value
-            end
-        end
-    end
+
 end
