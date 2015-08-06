1320931d60b5f5a9122509d17cbd9dd27646ca0c
Paul Rose <mail@paulrose.me>
2015-08-06 18:38:25 +0100

    Added widget loading code with job support

--- lib/bmdash/core.rb
+++ lib/bmdash/core.rb
@@ -1,11 +1,14 @@
 require 'sinatra'
+require 'sinatra/json'
 require 'sprockets'
 require 'sass'
 require 'rufus-scheduler'
 require 'filewatcher'
 require 'yaml'
+require 'json'
+require 'logger'
 require 'pp'
-
+require_relative '../script/lib/script.rb'
 
 module Bytemark
     class BMDash < Sinatra::Base
@@ -20,28 +23,73 @@ module Bytemark
         end
 
         class Widget 
-            attr_reader :name, :author, :email, :about
+            attr_reader :name, :author, :email, :about, :html
             def initialize about
                 about.each do |key,value|
                    instance_variable_set "@#{key}", value
                 end
             end
+
+            def to_json options
+                hash = {}
+                instance_variables.each do |var|
+                    key = var.to_s
+                    key[0] = ''
+                    hash[key] = instance_variable_get var
+                end
+                JSON.pretty_generate hash
+            end
         end
 
         def self.load_widgets
+            self.widgets = {}
             Dir.foreach('widgets') do |dir|
                 next if dir[0] == '.'
-                widget_info = File.join Dir.pwd, 'widgets',  dir, 'about.yml'
-                if File.exists? widget_info 
-                   info = YAML::load_file widget_info
-                   self.widgets[info['name']] = Widget.new(info)
+                widget_dir = File.join Dir.pwd, 'widgets',  dir
+                widget_info = File.join widget_dir, 'about.yml'
+
+                info = YAML::load_file widget_info
+
+                widget_html = File.join widget_dir, "#{info['name']}.html"
+                widget_job = File.join widget_dir, "#{info['name']}.rb"
+
+                # Check that about.yml exists, skip if not
+                if ! File.exists? widget_html 
+                    self.logger.warn "Widget #{info['name']} has no about.yml, skipping..."
+                    next
+                end
+                # Check that the html file exists, skip if not
+                if ! File.exists? widget_info 
+                    self.logger.warn "Widget #{info['name']} does not have a #{info['name']}.html file, skipping..."
+                    next
+                end
+
+                info['url'] = "widgets/#{info['name']}.html"
+
+                if File.exists? widget_job
+                    info['job'] = true
+                    self.jobs[info['name']] = Script.load(widget_job) do |script|
+                        script.__send__(:attr_accessor, 'scheduler', 'logger');
+                        script.__send__('scheduler=', self.scheduler)
+                        script.__send__('logger=', self.logger)
+                    end
+                else
+                    info['job'] = false
+                end
+                
+                self.widgets[info['name']] = Widget.new(info)
+                self.asset_types.each do |asset_dir|
+                   asset_path = File.join widget_dir, asset_dir
+                   if Dir.exists? asset_path
+                       self.settings.assets.append_path(asset_path)
+                   end
                 end
             end
-            puts 'Available widgets:'
-            self.widgets.each do |name, widget|
-                puts "    - #{name}"
-            end
 
+            self.logger.info 'Available widgets:'
+            self.widgets.each do |name, widget|
+                self.logger.info "    - #{name}"
+            end
         end
 
         def self.file_changed filename, event
@@ -49,29 +97,43 @@ module Bytemark
         end
 
         def self.ping_clients 
-            puts 'Currently Connected: '
+            self.logger.info 'Currently Connected: '
             self.connections.each do |client|
-                puts '    - ' + client.name
+                self.logger.info '    - ' + client.name
                 client.stream << "ping!"
             end
         end
 
+        def self.setup_widgets
+            self.jobs.each do |widget_name, job|
+                begin
+                    job.setup
+                rescue NoMethodError
+                    self.logger.warn "#{widget_name} has no run method!"
+                end
+            end
+        end
+
         configure do 
-            # Enable sweet things
-            enable :logging
+
+            # Deal with logging
+            logger = Logger.new(STDOUT)
+
+            set :logger, logger
 
             # Setup defaults 
+            set :server, 'thin'
             set :root, Dir.pwd
             set :public_folder, File.join(settings.root, 'public')
             set :assets, Sprockets::Environment.new(settings.root)
+            set :asset_types, %w(javascripts stylesheets fonts images sounds)
             set :scheduler, Rufus::Scheduler.new 
             set :watcher, FileWatcher.new(['./widgets', './dashboards'])
             set :watcher_thread, nil
             set :connections, []
+            set :jobs, {}
             set :widgets, {}
-            set :server, 'thin'
-            set :logger, ENV['rack.logger'] # Setup asset processing
-            %w(javascripts stylesheets fonts images sounds).each do |path|
+            asset_types.each do |path|
                settings.assets.append_path("./assets/#{path}")
             end
             manifest = Sprockets::Manifest.new(assets, './public/assets/manifest.json').save
@@ -83,13 +145,16 @@ module Bytemark
             scheduler.every '5s' do 
                ping_clients
             end
-            
+
             # Setup file watching
             watcher_thread = Thread.new(watcher) do |watcher|
                 watcher.watch do |filename, event|
                     self.file_changed filename, event
                 end
             end
+
+            # Run the setup methods in all the scripts 
+            setup_widgets
 
         end
 
@@ -104,9 +169,17 @@ module Bytemark
 
         get '/assets/:asset' do 
              asset =  settings.assets.find_asset(params[:asset])
-             content_type asset.content_type
-             asset
+             if asset 
+                 content_type asset.content_type
+                 asset
+             else
+                404
+             end
         end 
+
+        get '/widgets' do 
+                json settings.widgets
+        end
 
         get '/events', provides: 'text/event-stream' do
           return 403 if params[:name].nil? || params[:type].nil?
@@ -123,15 +196,16 @@ module Bytemark
                 settings.connections.delete(client)
             end
             settings.connections << client
+            logger.info "Client #{client.name} has connected!"
             
             out << "Hello!"
           end
         end
 
         get '/' do 
+            settings.logger.info "Yeah!"
             'BMDash! is here'
         end
     end
-
 
 end
