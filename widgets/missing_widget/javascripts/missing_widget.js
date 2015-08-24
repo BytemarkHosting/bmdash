9d6a9a7805ad3618b48d56c17bee50baddaaf29e
Paul Rose <mail@paulrose.me>
2015-08-24 18:09:00 +0100

    Added Missing missing widget

---
+++ widgets/missing_widget/javascripts/missing_widget.js
@@ -0,0 +1,1 @@
+console.out("test_widget has been loaded")
---
+++ widgets/missing_widget/missing_widget.rb
@@ -0,0 +1,19 @@
+def setup 
+    @logger.info 'This is code from  test widget.  WOO!'
+end
+
+
+def some_method
+    @events.push({ 
+        :event => 'test_event' ,
+        :data => {
+            :a => 'This is A',
+            :b => 'This is B'
+        }
+    })
+end
+
+@scheduler.every '10s' do 
+    logger.info "test_widget has made an event" 
+    some_method
+end
