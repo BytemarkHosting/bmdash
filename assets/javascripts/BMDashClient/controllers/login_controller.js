4854b9af7b716928d763889183a64e7228580662
Paul Rose <paul.rose@bytemark.co.uk>
2016-01-06 11:49:45 +0000

    Added client autologin
    Login Controller will call get_dashboards if clients request / with parameters
    for username and group. If one is missing the login box will appear

--- assets/javascripts/BMDashClient/controllers/login_controller.js
+++ assets/javascripts/BMDashClient/controllers/login_controller.js
@@ -1,9 +1,10 @@
-BMDash.controller('Login', ['$scope', '$rootScope', 'EventStream', 
-    function($scope, $rootScope, stream){
+BMDash.controller('Login', 
+    ['$scope', '$rootScope', 'EventStream', 'ClientName', 'ClientGroup',
+    function($scope, $rootScope, stream, client, group){
 
         $scope.client = {
-            name: 'saki',
-            group: 'testing'
+            name: client,
+            group: group
         }
 
         $scope.get_dashboards = function(client){
@@ -14,6 +15,16 @@ BMDash.controller('Login', ['$scope', '$rootScope', 'EventStream',
                 $rootScope.$broadcast('getDashboards')
             });
         };
+
+        // Check to see if there is a predefined Client name, Group
+        // If there is then run get_dashboards without user action
+        var init = function() {
+             if (client.length > 0 && group.length > 0){
+                $scope.get_dashboards($scope.client);
+             }
+        }
+        init();
+
 
     }
 ]);
