448eec0d8325560e7aa0529dbec18067b6a86551
Paul Rose <paul.rose@bytemark.co.uk>
2016-01-06 16:42:55 +0000

    Change widget service to use sane promises

--- assets/javascripts/BMDashClient/services/widget_service.js
+++ assets/javascripts/BMDashClient/services/widget_service.js
@@ -1,21 +1,22 @@
 BMDash.service('WidgetService', ['$q','$interval', '$http', 
     function($q, $interval, $http){
 
-        this.deferred = $q.defer();
-        this.availableWidgets = this.deferred.promise;
-        this.lastUpdate = Date.now();
+        this.availableWidgets = null;
+        this.lastUpdate = null;
 
         this.update = function(){
+            var deferred = $q.defer();
+            this.availableWidgets = deferred.promise;
+
             $http.get('/widgets').then(
                 function(response){
-                    console.log('WIDGET SERVICE: Got widget data!');
-                    this.availableWidgets = response.data;
                     this.lastUpdate = Date.now();
-                    console.log(response.data);
+                    deferred.resolve(response.data);
+                    console.log('WIDGET SERVICE: Got widget data);
                 }, 
                 function(response){
+                    deferred.reject(null);
                     console.log('WIDGET SERVICE: Failed to get widget data from server!');
-                    this.reset();
                 }
             );
         }
