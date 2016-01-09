1b25b2af3615269f8da413952a1a35713ccd87f4
Sherman Rose <sherman@bytemark.co.uk>
2016-01-09 11:18:22 +0000

    Changed login controller to use BMDashService

--- assets/javascripts/BMDashClient/controllers/login_controller.js
+++ assets/javascripts/BMDashClient/controllers/login_controller.js
@@ -1,6 +1,6 @@
 BMDash.controller('Login', 
-    ['$scope', '$rootScope', 'EventStream', 'ClientName', 'ClientGroup',
-    function($scope, $rootScope, stream, client, group){
+    ['$scope', '$rootScope','ClientName', 'ClientGroup', 'BMDashService',
+    function($scope, $rootScope, client, group, bmDashService){
 
         $scope.connected = false;
 
@@ -10,13 +10,7 @@ BMDash.controller('Login',
         }
 
         $scope.connect = function(client){
-            console.log('LOGIN: Creating connection for ' + client.name)
-            stream.connect(client.name, client.group)
-            .then(function(){
-                console.log("LOGIN: Connection Succesful!");
-                $scope.connected = true;
-                $rootScope.$broadcast('ClientConnected')
-            });
+            bmDashService.connect(client.name, client.group);
         };
 
         // Check to see if there is a predefined Client name, Group
