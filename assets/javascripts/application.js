b90a01f1d115ae79b5edcd39aca1a3f390671777
Telyn ZF Roat <telyn.roat@bytemark.co.uk>
2016-03-02 14:51:12 +0000

    Load angular after jQuery. This makes script tags work

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,6 @@
-//= require vendor/angular
 //= require vendor/jquery-2.1.4.min
 //= require vendor/jquery-ui
 //= require vendor/gridList
 //= require vendor/jquery.gridList
+//= require vendor/angular
 //= require_tree ./BMDashClient 
